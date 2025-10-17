3db7877710d64e1415b1baf5db21039bc0c450d3
Martin Donadieu <martindonadieu@gmail.com>
2025-10-17 13:36:57 +0100

    fix: kill delay issue

--- android/src/main/java/ee/forgr/capacitor_updater/CapacitorUpdaterPlugin.java
+++ android/src/main/java/ee/forgr/capacitor_updater/CapacitorUpdaterPlugin.java
@@ -195,7 +195,6 @@ public class CapacitorUpdaterPlugin extends Plugin {
                 this.prefs,
                 this.editor,
                 this.currentVersionNative,
-                CapacitorUpdaterPlugin.this::installNext,
                 logger
             );
         } catch (final PackageManager.NameNotFoundException e) {
